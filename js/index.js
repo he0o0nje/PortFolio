173319aff7e848c1ff9cc3668ebcc6b8d217092f
he0o0nje <dudwogjswp@naver.com>
2023-11-15 14:45:45 +0900

    11-15

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $(function () {
   /*첫번째 웹페이지 */
   $(".webpage01")
     .mouseenter(function () {
-      $(".webpage01 img").stop().animate({ marginTop: -3200 }, 4000);
+      $(".webpage01 img").stop().animate({ marginTop: -3070 }, 4000);
     })
     .mouseleave(function () {
       $(".webpage01 img").stop().animate({ marginTop: 0 }, 2000);
@@ -23,7 +23,7 @@ $(function () {
   /*두번째 웹페이지 */
   $(".webpage02")
     .mouseenter(function () {
-      $(".webpage02 img").stop().animate({ marginTop: -2400 }, 3000);
+      $(".webpage02 img").stop().animate({ marginTop: -1570 }, 3000);
     })
     .mouseleave(function () {
       $(".webpage02 img").stop().animate({ marginTop: 0 }, 2000);
@@ -32,7 +32,7 @@ $(function () {
   /*세번째 웹페이지 */
   $(".webpage03")
     .mouseenter(function () {
-      $(".webpage03 img").stop().animate({ marginTop: -2700 }, 3000);
+      $(".webpage03 img").stop().animate({ marginTop: -980 }, 3000);
     })
     .mouseleave(function () {
       $(".webpage03 img").stop().animate({ marginTop: 0 }, 2000);
@@ -41,13 +41,13 @@ $(function () {
   /*네번째 웹페이지 */
   $(".webpage04")
     .mouseenter(function () {
-      $(".webpage04 img").stop().animate({ marginTop: -2000 }, 3000);
+      $(".webpage04 img").stop().animate({ marginTop: -1830 }, 3000);
     })
     .mouseleave(function () {
       $(".webpage04 img").stop().animate({ marginTop: 0 }, 2000);
     });
 
-  /*네번째 웹페이지 */
+  /*다섯번째 웹페이지 */
   $(".webpage05")
     .mouseenter(function () {
       $(".webpage05 img").stop().animate({ marginTop: -2000 }, 3000);
